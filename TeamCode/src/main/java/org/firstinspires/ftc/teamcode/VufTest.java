a8f9968c77f1eac5d5b6cce074c43f01897158b3
RazrSlyr <sharpedo75@gmail.com>
2017-09-21 16:48:38 -0700

    Created the final vuforia recognition code for the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VufTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VufTest.java
@@ -128,7 +128,7 @@ public class VufTest extends LinearOpMode {
                 /* Found an instance of the template. In the actual game, you will probably
                  * loop until this condition occurs, then move on to act accordingly depending
                  * on which VuMark was visible. */
-                telemetry.addData("VuMark", vuMark.name() + "is visible");
+                telemetry.addData("VuMark", vuMark.name() + " is visible");
 
                 /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
                  * it is perhaps unlikely that you will actually need to act on this pose information, but

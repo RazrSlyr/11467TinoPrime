c030ae415c8e31ab55b602d9c0ea3ff15c2a0948
RazrSlyr <sharpedo75@gmail.com>
2017-10-03 15:59:36 -0700

    color test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by guydu on 10/3/2017.
+ */
+
+public class ColorTest extends OpMode{
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop() {
+
+    }
+}

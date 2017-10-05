9e2b742d2f283864f10ebb4212ca7fad10ac70a3
kristinemclaughlin <kristinesoohyun@yahoo.com>
2017-10-05 15:55:30 -0700

    Servo Test

--- TeamCode/src/main/java/ServoTest.java
+++ TeamCode/src/main/java/ServoTest.java
@@ -1,6 +1,22 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.MotorType;
+import com.qualcomm.robotcore.hardware.configuration.UnspecifiedMotor;
+
 /**
  * Created by kristinemclaughlin on 10/5/17.
  */
 
-public class ServoTest {
+public class ServoTest extends LinearOpMode{
+    Servo test;
+    DcMotor dcMotor;
+    @Override
+    public void runOpMode() {
+        test = hardwareMap.servo.get("servo");
+        while (opModeIsActive()){
+
+
+        }
+    }
 }

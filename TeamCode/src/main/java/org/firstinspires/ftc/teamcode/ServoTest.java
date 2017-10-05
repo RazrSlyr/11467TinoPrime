96ec1bc397549dd061250ab9bed4868e4e51c105
RazrSlyr <sharpedo75@gmail.com>
2017-10-05 16:04:32 -0700

    servo stuff made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,14 +11,17 @@ import com.qualcomm.robotcore.hardware.configuration.UnspecifiedMotor;
  */
 
 public class ServoTest extends LinearOpMode{
-    Servo test;
+    Servo servo;
     DcMotor dcMotor;
     @Override
     public void runOpMode() {
-        test = hardwareMap.servo.get("servo");
+        servo = hardwareMap.servo.get("servo");
         while (opModeIsActive()){
-
-
+            servo.setPosition(0.5);
+            long time = System.currentTimeMillis();
+            while(System.currentTimeMillis() - time < 1000 && opModeIsActive()) continue;
+            servo.setPosition(0);
+            stop();
         }
     }
 }

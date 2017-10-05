3fb90563b9ff7bc2ea89a12afb64faba50b7fbe7
RazrSlyr <sharpedo75@gmail.com>
2017-10-05 16:42:06 -0700

    fixed issues with servo and added telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,18 +10,27 @@ import com.qualcomm.robotcore.hardware.configuration.UnspecifiedMotor;
 /**
  * Created by kristinemclaughlin on 10/5/17.
  */
-
+@Autonomous(name="Servo Test", group="")
 public class ServoTest extends LinearOpMode{
     Servo servo;
     DcMotor dcMotor;
     @Override
     public void runOpMode() {
         servo = hardwareMap.servo.get("servo");
+        servo.setPosition(0);
+
+        waitForStart();
+
         while (opModeIsActive()){
             servo.setPosition(0.5);
+            telemetry.addData("Servo Position Step 1", servo.getPosition());
             long time = System.currentTimeMillis();
             while(System.currentTimeMillis() - time < 1000 && opModeIsActive()) continue;
             servo.setPosition(0);
+            telemetry.addData("Servo Position Step 2", servo.getPosition());
+            while(System.currentTimeMillis() - time < 2000 && opModeIsActive()) continue;
+            telemetry.update();
+            while(System.currentTimeMillis() - time < 4000 && opModeIsActive()) continue;
             stop();
         }
     }

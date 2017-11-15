e71c342361d60b9c5c39248ec6e71eee88e9a147
Gargi <gargid03@gmail.com>
2017-11-14 16:15:13 -0800

    run code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurnTest.java
@@ -16,82 +16,9 @@ public class TurnTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot();
         robot.setHardwareMap(hardwareMap);
-        final GyroSensor gyroSensor = new GyroSensor() {
-            @Override
-            public void calibrate() {
 
-            }
 
-            @Override
-            public boolean isCalibrating() {
-                return false;
-            }
 
-            @Override
-            public int getHeading() {
-                return 0;
-            }
-
-            @Override
-            public double getRotationFraction() {
-                return 0;
-            }
-
-            @Override
-            public int rawX() {
-                return 0;
-            }
-
-            @Override
-            public int rawY() {
-                return 0;
-            }
-
-            @Override
-            public int rawZ() {
-                return 0;
-            }
-
-            @Override
-            public void resetZAxisIntegrator() {
-
-            }
-
-            @Override
-            public String status() {
-                return null;
-            }
-
-            @Override
-            public Manufacturer getManufacturer() {
-                return null;
-            }
-
-            @Override
-            public String getDeviceName() {
-                return null;
-            }
-
-            @Override
-            public String getConnectionInfo() {
-                return null;
-            }
-
-            @Override
-            public int getVersion() {
-                return 0;
-            }
-
-            @Override
-            public void resetDeviceConfigurationForOpMode() {
-
-            }
-
-            @Override
-            public void close() {
-
-            }
-        }
     }
 
 }
